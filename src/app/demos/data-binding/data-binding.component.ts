b526e50c11a0757c4a509d631e37f47b4fb507d9
Samory Sundjata <samory.sundjata@gmail.com>
2022-10-25 13:46:20 -0300

    first commit

---
+++ src/app/demos/data-binding/data-binding.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styles: [
+  ]
+})
+export class DataBindingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
